d343621262e9b7cc1b64bfab074c1f7bf968f1a1
Vladyslav <vladyslav@Vladyslavs-MacBook-Pro.local>
2017-11-19 21:08:32 +0200

    add test take_money for Game model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -72,4 +72,21 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.finished?).to be_falsey
     end
   end
+
+  it 'take_money! finishes the game' do
+    # берем игру и отвечаем на текущий вопрос
+    q = game_w_questions.current_game_question
+    game_w_questions.answer_current_question!(q.correct_answer_key)
+
+    # взяли деньги
+    game_w_questions.take_money!
+
+    prize = game_w_questions.prize
+    expect(prize).to be > 0
+
+    # проверяем что закончилась игра и пришли деньги игроку
+    expect(game_w_questions.status).to eq :money
+    expect(game_w_questions.finished?).to be_truthy
+    expect(user.balance).to eq prize
+  end
 end
