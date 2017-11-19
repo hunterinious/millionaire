1ada1fb9803961e13877beb49274b793b81219f9
Vladyslav <vladyslav@Vladyslavs-MacBook-Pro.local>
2017-11-19 21:07:35 +0200

    add some tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -12,8 +12,12 @@ require 'rails_helper'
 # валидаций.
 RSpec.describe Question, type: :model do
   context 'validations check' do
+
+    subject { Question.new(text: 'Hello world', level: 0, answer1: '1', answer2: '2', answer3: '3', answer4: '4') }
+
     it { should validate_presence_of :level }
     it { should validate_presence_of :text }
+    it { should validate_uniqueness_of(:text) }
     it { should validate_inclusion_of(:level).in_range(0..14) }
 
     it { should_not allow_value(500).for(:level) }
