464f935e2e40f9908546a8c9290627a44504d7b7
Vladyslav <vladyslav@Vladyslavs-MacBook-Pro.local>
2017-11-20 20:37:25 +0200

    add tests for anon user

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -28,6 +28,36 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path) # devise должен отправить на логин
       expect(flash[:alert]).to be # во flash должен быть прописана ошибка
     end
+
+    it 'kicked from #create' do
+      generate_questions(15)
+      post :create
+      game = assigns(:game)
+
+      expect(game).to be_nil
+      expect(response.status).not_to eq 200
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'kicked from answer' do
+      game =  FactoryGirl.create(:game_with_questions)
+      put :answer, id: game.id, letter: game.current_game_question.correct_answer_key
+
+      expect(response.status).not_to eq 200
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
+
+    it 'kicked from take_money' do
+      game =  FactoryGirl.create(:game_with_questions)
+
+      put :take_money, id: game.id
+
+      expect(response.status).not_to eq 200
+      expect(response).to redirect_to(new_user_session_path)
+      expect(flash[:alert]).to be
+    end
   end
 
   # группа тестов на экшены контроллера, доступных залогиненным юзерам
