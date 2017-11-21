9303d6a644c5a33c378386fc08e16754c00b9b6c
Vladyslav <vladyslav@Vladyslavs-MacBook-Pro.local>
2017-11-21 20:12:59 +0200

    add friend_call help test

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -84,10 +84,20 @@ RSpec.describe GameQuestion, type: :model do
 
       game_question.add_fifty_fifty
 
-      ff = game_question.help_hash[:fifty_fifty_help]
+      ff = game_question.help_hash[:fifty_fifty]
       expect(game_question.help_hash).to include(:fifty_fifty)
       expect(ff).to include('b')
       expect(ff.size).to eq 2
+    end
+
+    it 'correct friend_call_help' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+
+      game_question.add_friend_call
+
+      expect(game_question.help_hash).to include(:friend_call)
+      fc = game_question.help_hash[:friend_call]
+      expect(fc).to be
     end
   end
 end
